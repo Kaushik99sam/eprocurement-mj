916443797413e7ed3927b61acdbb65e99eafeaf0
Harsh Gupta <Harsh.Gupta@mjunction.in>
2023-10-11 16:57:27 +0530

    Table-corrected

--- navbar/src/components/Tenders/Table.jsx
+++ navbar/src/components/Tenders/Table.jsx
@@ -1,130 +1,3 @@
-// import React from "react";
-
-// import { useTable, useSortBy, usePagination } from 'react-table';
-// import Data from '../../data/data.json'
-// import { AiFillCaretLeft, AiFillCaretRight } from 'react-icons/ai'
-// import { FaCaretDown, FaCaretUp } from 'react-icons/fa6'
-// import './Table.css';
-
-// function Table({ columns, data }) {
-//     // Use the state and functions returned from useTable to build your UI
-//     const {
-//         getTableProps,
-//         getTableBodyProps,
-//         headerGroups,
-//         page,
-//         nextPage,
-//         previousPage,
-//         pageOptions,
-//         state,
-//         prepareRow,
-//         canPreviousPage,
-//         canNextPage,
-//     } = useTable(
-//         {
-//             columns,
-//             data,
-//         },
-//         useSortBy,
-//         usePagination
-//     )
-
-//     const { pageIndex } = state
-
-//     // Render the UI for your table
-//     return (
-//         <div className="absolute">
-//             <table className="table" {...getTableProps()}>
-//                 <thead className="color-change">
-//                     {headerGroups.map(headerGroup => (
-//                         <tr {...headerGroup.getHeaderGroupProps()}>
-//                             {headerGroup.headers.map(column => (
-//                                 // Add the sorting props to control sorting. For this example
-//                                 // we can add them into the header props
-//                                 <th {...column.getHeaderProps(column.getSortByToggleProps())}>
-//                                     {column.render('Header')}
-//                                     {/* Add a sort direction indicator */}
-//                                     <span>
-//                                         &nbsp;
-//                                         {column.isSorted
-//                                             ? column.isSortedDesc
-//                                                 ? <FaCaretDown />
-//                                                 : <FaCaretUp />
-//                                             : ''}
-//                                     </span>
-//                                 </th>
-//                             ))}
-//                         </tr>
-//                     ))}
-//                 </thead>
-//                 <tbody {...getTableBodyProps()}>
-//                     {page.map(
-//                         (row, i) => {
-//                             prepareRow(row);
-//                             return (
-//                                 <tr {...row.getRowProps()}>
-//                                     {row.cells.map(cell => {
-//                                         console.log(cell)
-//                                         return (
-//                                             <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-//                                         )
-//                                     })}
-//                                 </tr>
-//                             )
-//                         }
-//                     )}
-//                 </tbody>
-//             </table>
-//             <br />
-//             <div>
-//                 <span>
-//                     Page{''}
-//                     <strong>
-//                         {pageIndex + 1} of {pageOptions.length}
-//                     </strong>
-//                 </span>&nbsp;
-//                 <button onClick={() => previousPage()} disabled={!canPreviousPage} className="btn btn-sm color"><AiFillCaretLeft /> Previous</button>&nbsp;
-//                 <button onClick={() => nextPage()} disabled={!canNextPage} className="btn btn-sm color">Next <AiFillCaretRight /></button>
-//             </div>
-//         </div>
-//     )
-// }
-
-// function SortingTableComponent() {
-//     const columns = React.useMemo(
-//         () => [
-
-//             {
-//                 Header: 'RFP ID',
-//                 accessor: 'id',
-//             },
-//             {
-//                 Header: 'Description',
-//                 accessor: 'description',
-//             },
-//             {
-//                 Header: 'Status',
-//                 accessor: 'status',
-//                 cell: (value) => {
-//                     if (value === 'open')
-//                         return (<button>Open</button>);
-//                 }
-//             }
-//         ],
-//         []
-//     )
-//     // console.log(JSON.stringify(data));
-
-
-//     return (
-//         <Table columns={columns} data={Data} />
-//     )
-// }
-
-// /************************************************************************/
-
-// export default SortingTableComponent;
-
 import React, { useState } from "react";
 
 import {
@@ -135,53 +8,68 @@ import {
   flexRender,
   getSortedRowModel,
 } from "@tanstack/react-table";
-
-import Data from '../../data/data.json'
+import { FaCaretDown, FaCaretUp } from 'react-icons/fa6'
 
 export default function TableFun() {
+  const Data = [
+    {
+      "id": "committee-c15dw",
+      "description": "editor-ktsjo",
+      "status": "closed"
+    },
+    {
+      "id": "midnight-wad0y",
+      "description": "data-7h4xf",
+      "status": "closed"
+    },
+    {
+      "id": "tree-sbdb0",
+      "description": "friendship-w8535",
+      "status": "open"
+    },
+    {
+      "id": "chin-borr8",
+      "description": "shirt-zox8m",
+      "status": "closed"
+    },
+    {
+      "id": "women-83ef0",
+      "description": "chalk-e8xbk",
+      "status": "closed"
+    },
+    {
+      "id": "women-83ef0",
+      "description": "chalk-e8xbk",
+      "status": "closed"
+    }
+  ]
   const rerender = React.useReducer(() => ({}), {})[1];
-  
 
   const columns = React.useMemo(
     () => [
       {
+        header:"Id",
         accessorKey: "id",
         cell: (info) => info.getValue(),
         footer: (props) => props.column.id,
       },
-    //   {
-    //     accessorFn: (row) => row.lastName,
-    //     id: "lastName",
-    //     cell: (info) => info.getValue(),
-    //     header: () => <span>Last Name</span>,
-    //     footer: (props) => props.column.id,
-    //   },
-    //   {
-    //     accessorKey: "age",
-    //     header: () => "Age",
-    //     footer: (props) => props.column.id,
-    //   },
-    //   {
-    //     accessorKey: "visits",
-    //     header: () => <span>Visits</span>,
-    //     footer: (props) => props.column.id,
-    //   },
       {
-        accessorKey: "description",
-        header: "Status",
+        accessorKey:"description",
+        cell: (info) => info.getValue(),
+        header: () => "Last Name",
         footer: (props) => props.column.id,
       },
       {
-        accessorKey: "progress",
-        header: "Profile Progress",
+        accessorKey: "status",
+        header: () => "Age",
         footer: (props) => props.column.id,
       },
     ],
     []
   );
 
-  const [data, setData] = React.useState(() => makeData(100000));
-  const refreshData = () => setData(() => makeData(100000));
+  const [data, setData] = React.useState(() => Data);
+  const refreshData = () => setData(() => Data);
 
   return (
     <>
@@ -208,7 +96,11 @@ function Table({ data, columns }) {
     data,
     columns,
     state: {
-      sorting
+      sorting,
+      pagination:{
+        pageIndex:0,
+        pageSize:3,
+      }
     },
     // Pipeline
     onSortingChange: setSorting,
@@ -220,10 +112,12 @@ function Table({ data, columns }) {
     debugTable: true,
   });
 
+  // console.log(table.nextPage())
+
   return (
     <div className="p-2">
       <div className="h-2" />
-      <table>
+      <table className="table">
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id}>
@@ -242,8 +136,8 @@ function Table({ data, columns }) {
                           header.getContext()
                         )}
                         {{
-                          asc: " 🔼",
-                          desc: " 🔽"
+                          asc: <FaCaretUp/>,
+                          desc: <FaCaretDown/>
                         }[header.column.getIsSorted()] ?? null}
                       </div>
                     )}
@@ -311,44 +205,5 @@ function Table({ data, columns }) {
         </span>
       </div>
     </div>
-  );
-}
-
-function Filter({ column, table }) {
-  const firstValue = table
-    .getPreFilteredRowModel()
-    .flatRows[0]?.getValue(column.id);
-
-  const columnFilterValue = column.getFilterValue();
-
-  return typeof firstValue === "number" ? (
-    <div className="flex space-x-2">
-      <input
-        type="number"
-        value={columnFilterValue?.[0] ?? ""}
-        onChange={(e) =>
-          column.setFilterValue((old) => [e.target.value, old?.[1]])
-        }
-        placeholder={`Min`}
-        className="w-24 border shadow rounded"
-      />
-      <input
-        type="number"
-        value={columnFilterValue?.[1] ?? ""}
-        onChange={(e) =>
-          column.setFilterValue((old) => [old?.[0], e.target.value])
-        }
-        placeholder={`Max`}
-        className="w-24 border shadow rounded"
-      />
-    </div>
-  ) : (
-    <input
-      type="text"
-      value={columnFilterValue ?? ""}
-      onChange={(e) => column.setFilterValue(e.target.value)}
-      placeholder={`Search...`}
-      className="w-36 border shadow rounded"
-    />
   );
 }
