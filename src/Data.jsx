4e85a3a90eb7b17cc07eca14918060109b623518
Abhineet Kumar <Abhineet.Kumar@mjunction.in>
2023-10-12 17:59:56 +0530

    first commit

---
+++ src/Data.jsx
@@ -0,0 +1,17 @@
+export const userList = [
+    {
+        name: "BatMan",
+        email: "batman@gmail.com",
+        id: 1
+    },
+    {
+        name: "Superman",
+        email: "superman@gmail.com",
+        id: 2
+    },
+    {
+        name: "WonderWomen",
+        email: "wonderwomen@gmail.com",
+        id: 3
+    }
+]
