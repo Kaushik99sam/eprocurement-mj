4aeff4d6a2346d725e1ce749f3eeaed843de600b
Kaushik <kausik99sam@gmail.com>
2023-10-29 22:22:47 +0530

    backend edited

--- src/main/java/com/mjPro/EvalPoApplication.java
+++ src/main/java/com/mjPro/EvalPoApplication.java
@@ -2,9 +2,10 @@ package com.mjPro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EvalPoApplication {
+public class EvalPoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EvalPoApplication.class, args);
